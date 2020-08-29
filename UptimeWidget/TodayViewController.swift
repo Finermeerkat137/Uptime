6f114a4a8e6b7558fa7235bca10f3748c9c5c8fa
sudhip nashi <sudhip@nashi.us>
2020-08-29 18:09:58 -0500

    start on Today Widget

--- UptimeWidget/TodayViewController.swift
+++ UptimeWidget/TodayViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-    @IBOutlet weak var rmMe: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dayText: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var hourText: UILabel!
+    @IBOutlet weak var minuteLabel: UILabel!
+    @IBOutlet weak var minuteText: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    var refreshTimer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        rmMe.text = String(get_seconds())
+        center()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        refreshWithTimer()
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
@@ -25,6 +37,29 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // If there's an update, use NCUpdateResult.NewData
         
         completionHandler(NCUpdateResult.newData)
+    }
+    
+    func refreshWithTimer() {
+        refreshTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.refresh), userInfo: nil, repeats: true)
+    }
+    
+    @objc func refresh() {
+        dayLabel.text = String(get_days())
+        hourLabel.text = String(get_hours())
+        minuteLabel.text = String(get_minutes())
+        secondLabel.text = String(get_seconds())
+    }
+    
+    func center() {
+        dayLabel.center.y = self.view.center.y
+        dayText.center.y = self.view.center.y
+        hourLabel.center.y = self.view.center.y
+        hourText.center.y = self.view.center.y
+        minuteLabel.center.y = self.view.center.y
+        minuteText.center.y = self.view.center.y
+        minuteLabel.center.y = self.view.center.y
+        secondLabel.center.y = self.view.center.y
+        
     }
     
 }
