632e9d28594cbd72c873f0378af9feb951489da1
sudhip nashi <sudhip@nashi.us>
2020-08-28 23:54:26 -0500

    add widget folder again

---
+++ UptimeWidget/Uptime-Bridging-Header.c
@@ -0,0 +1,17 @@
+//
+//  Uptime-Bridging-Header.c
+//  UptimeWidget
+//
+//  Created by Sudhip Nashi on 8/28/20.
+//  Copyright © 2020 nashi. All rights reserved.
+//
+
+struct timespec get_uptime(void);
+
+unsigned long get_seconds(void);
+
+unsigned long get_minutes(void);
+
+unsigned long get_hours(void);
+
+unsigned long get_days(void);
