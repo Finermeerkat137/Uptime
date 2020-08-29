606255ddb2f5af2c620581936f88eff11ba31c2b
sudhip nashi <sudhip@nashi.us>
2020-08-29 00:56:05 -0500

    remove unnecessary headers

--- UptimeWidget/Uptime-Bridging-Header.h
+++ UptimeWidget/Uptime-Bridging-Header.h
@@ -1,10 +1,13 @@
 //
-//  Uptime-Bridging-Header.c
+//  Uptime-Bridging-Header.h
 //  UptimeWidget
 //
 //  Created by Sudhip Nashi on 8/28/20.
 //  Copyright © 2020 nashi. All rights reserved.
 //
+
+#ifndef Uptime_Bridging_Header_h
+#define Uptime_Bridging_Header_h
 
 struct timespec get_uptime(void);
 
@@ -15,3 +18,5 @@ unsigned long get_minutes(void);
 unsigned long get_hours(void);
 
 unsigned long get_days(void);
+
+#endif /* Uptime_Bridging_Header_h */
